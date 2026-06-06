0ac206262c388b465910f9263cbba6188711f818
Héctor Albino López González <lopezgonzalezhectoralbino@gmail.com>
2026-06-06 15:44:51 -0600

    Version Final del proyecto. Sigue luz y tiene un sensor ultrasonico para esquivar obstaculos

--- fsm.vhd
+++ fsm.vhd
@@ -1,28 +1,38 @@
 -- ==========================================================
--- FSM - Robot seguidor de luz
--- FPGA: Intel DE10-Lite (MAX10, 50 MHz)
--- Reset: activo bajo (KEY0)
+-- FSM - SEGUIR luz con estado de esquive
+-- Estados:
+--   ESTADO_INICIO      : 1 segundo parado
+--   ESTADO_BUSCAR_LUZ  : detenido hasta detectar luz
+--   ESTADO_SEGUIR_LUZ  : mueve motores segun LDR
+--   ESTADO_ESQUIVE     : inicia con obstaculo a 20 cm o menos
+--   ESTADO_META        : parado al cumplir condicion de meta
 -- ==========================================================
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
 entity fsm is
+    generic (
+        CLK_FREQ_HZ     : positive := 50_000_000;
+        ESQUIVE_TIME_MS : positive := 650
+    );
     Port (
-        clk        : in  STD_LOGIC;
-        reset      : in  STD_LOGIC;
+        clk             : in  STD_LOGIC;
+        reset           : in  STD_LOGIC;
 
-        ldr_front  : in  STD_LOGIC;   -- PIN_W10
-        ldr_left   : in  STD_LOGIC;   -- PIN_V10
-        ldr_right  : in  STD_LOGIC;   -- PIN_W9
-        ldr_back   : in  STD_LOGIC;   -- PIN_V9
+        ldr_front       : in  STD_LOGIC;   -- PIN_W10
+        ldr_left        : in  STD_LOGIC;   -- PIN_V10
+        ldr_right       : in  STD_LOGIC;   -- PIN_W9
+        ldr_back        : in  STD_LOGIC;   -- PIN_V9
 
-        mot_l_a    : out STD_LOGIC;   -- PIN_AA15
-        mot_l_b    : out STD_LOGIC;   -- PIN_W13
-        mot_r_a    : out STD_LOGIC;   -- PIN_W5
-        mot_r_b    : out STD_LOGIC;   -- PIN_AA14
+        obstaculo_20cm  : in  STD_LOGIC;   -- '1' si hay objeto a 20 cm o menos
 
-        leds_state : out STD_LOGIC_VECTOR(2 downto 0)
+        mot_l_a         : out STD_LOGIC;   -- PIN_AA15 IN1
+        mot_l_b         : out STD_LOGIC;   -- PIN_W13  IN2
+        mot_r_a         : out STD_LOGIC;   -- PIN_W5   IN3
+        mot_r_b         : out STD_LOGIC;   -- PIN_AA14 IN4
+
+        leds_state      : out STD_LOGIC_VECTOR(2 downto 0)
     );
 end fsm;
 
@@ -34,8 +44,10 @@ architecture Behavioral of fsm is
         ESTADO_INICIO,
         ESTADO_BUSCAR_LUZ,
         ESTADO_SEGUIR_LUZ,
+        ESTADO_ESQUIVE,
         ESTADO_META
     );
+
     signal estado : state_type := ESTADO_INICIO;
 
     constant ARRANQUE_MAX : integer := 50_000_000;
@@ -46,16 +58,22 @@ architecture Behavioral of fsm is
     signal db_l : integer range 0 to 2_500_000 := 0;
     signal db_r : integer range 0 to 2_500_000 := 0;
     signal db_b : integer range 0 to 2_500_000 := 0;
+
     signal luz_front : STD_LOGIC := '0';
     signal luz_left  : STD_LOGIC := '0';
     signal luz_right : STD_LOGIC := '0';
     signal luz_back  : STD_LOGIC := '0';
 
-begin
+    function ms_to_ticks(ms_value : positive) return natural is
+    begin
+        return (CLK_FREQ_HZ / 1_000) * ms_value;
+    end function;
 
-    -- ==========================================================
-    -- DEBOUNCE + NORMALIZACION LDR
-    -- ==========================================================
+    constant ESQUIVE_MAX : natural := ms_to_ticks(ESQUIVE_TIME_MS);
+    signal esquive_cnt : natural range 0 to ESQUIVE_MAX := 0;
+
+begin
+    -- debounce + normalizacion LDR
     process(clk, reset)
     begin
         if reset = '0' then
@@ -63,46 +81,67 @@ begin
             db_l <= 0; luz_left  <= '0';
             db_r <= 0; luz_right <= '0';
             db_b <= 0; luz_back  <= '0';
+
         elsif rising_edge(clk) then
 
             if ldr_front = LDR_ACTIVO then
-                if db_f < DB_MAX then db_f <= db_f + 1;
-                else luz_front <= '1'; end if;
-            else db_f <= 0; luz_front <= '0'; end if;
+                if db_f < DB_MAX then
+                    db_f <= db_f + 1;
+                else
+                    luz_front <= '1';
+                end if;
+            else
+                db_f <= 0;
+                luz_front <= '0';
+            end if;
 
             if ldr_left = LDR_ACTIVO then
-                if db_l < DB_MAX then db_l <= db_l + 1;
-                else luz_left <= '1'; end if;
-            else db_l <= 0; luz_left <= '0'; end if;
+                if db_l < DB_MAX then
+                    db_l <= db_l + 1;
+                else
+                    luz_left <= '1';
+                end if;
+            else
+                db_l <= 0;
+                luz_left <= '0';
+            end if;
 
             if ldr_right = LDR_ACTIVO then
-                if db_r < DB_MAX then db_r <= db_r + 1;
-                else luz_right <= '1'; end if;
-            else db_r <= 0; luz_right <= '0'; end if;
+                if db_r < DB_MAX then
+                    db_r <= db_r + 1;
+                else
+                    luz_right <= '1';
+                end if;
+            else
+                db_r <= 0;
+                luz_right <= '0';
+            end if;
 
             if ldr_back = LDR_ACTIVO then
-                if db_b < DB_MAX then db_b <= db_b + 1;
-                else luz_back <= '1'; end if;
-            else db_b <= 0; luz_back <= '0'; end if;
+                if db_b < DB_MAX then
+                    db_b <= db_b + 1;
+                else
+                    luz_back <= '1';
+                end if;
+            else
+                db_b <= 0;
+                luz_back <= '0';
+            end if;
 
         end if;
     end process;
 
-    -- ==========================================================
-    -- FSM: todo registrado, pines asignados directamente
-    --
-    -- PARADO  : mot_X_a='0'  mot_X_b='0'
-    -- ADELANTE: mot_X_a='1'  mot_X_b='0'
-    -- ATRAS   : mot_X_a='0'  mot_X_b='1'
-    --
-    -- Si las llantas van al reves de lo esperado,
-    -- intercambia los cables fisicos en el L298N.
-    -- ==========================================================
+
+    -- Prioridad de obstaculo:Si obstaculo_20cm = '1' durante BUSCAR_LUZ o SEGUIR_LUZ,entra a ESTADO_ESQUIVE.
+    --	ESTADO_ESQUIVE:
+    -- Solo activa un motor para girar hacia un lado.
+    -- Para girar al lado contrario, apaga el izquierdo y activa el derecho.
     process(clk, reset)
     begin
         if reset = '0' then
             estado       <= ESTADO_INICIO;
             arranque_cnt <= 0;
+            esquive_cnt  <= 0;
             mot_l_a      <= '0';
             mot_l_b      <= '0';
             mot_r_a      <= '0';
@@ -110,102 +149,134 @@ begin
             leds_state   <= "000";
 
         elsif rising_edge(clk) then
-            case estado is
 
-            -- ================================================
-            -- INICIO: 1 segundo parado antes de arrancar
-            -- ================================================
-            when ESTADO_INICIO =>
-                leds_state <= "000";
-                mot_l_a    <= '0';
+            if (estado = ESTADO_BUSCAR_LUZ or estado = ESTADO_SEGUIR_LUZ) and
+               (obstaculo_20cm = '1') then
+
+                estado      <= ESTADO_ESQUIVE;
+                esquive_cnt <= 0;
+
+                -- Un solo motor activo: motor izquierdo adelante.
+                mot_l_a    <= '1';
                 mot_l_b    <= '0';
                 mot_r_a    <= '0';
                 mot_r_b    <= '0';
+                leds_state <= "011";
 
-                if arranque_cnt = ARRANQUE_MAX then
-                    arranque_cnt <= 0;
-                    estado       <= ESTADO_BUSCAR_LUZ;
-                else
-                    arranque_cnt <= arranque_cnt + 1;
-                end if;
+            else
+                case estado is
+                    when ESTADO_INICIO =>
+                        leds_state <= "000";
+                        mot_l_a    <= '0';
+                        mot_l_b    <= '0';
+                        mot_r_a    <= '0';
+                        mot_r_b    <= '0';
+                        esquive_cnt <= 0;
 
-            -- ================================================
-            -- BUSCAR_LUZ: gira sobre el eje
-            -- Motor izq ADELANTE, motor der ATRAS
-            -- Si el carro avanza en vez de girar:
-            --   cambia mot_r_b a '0' y mot_l_b a '1'
-            -- ================================================
-            when ESTADO_BUSCAR_LUZ =>
-    leds_state <= "001";
+                        if arranque_cnt = ARRANQUE_MAX then
+                            arranque_cnt <= 0;
+                            estado       <= ESTADO_BUSCAR_LUZ;
+                        else
+                            arranque_cnt <= arranque_cnt + 1;
+                        end if;
 
-    -- ROBOT DETENIDO MIENTRAS ESPERA LUZ
-    mot_l_a <= '0';
-    mot_l_b <= '0';
-    mot_r_a <= '0';
-    mot_r_b <= '0';
-		  if luz_front='1' or luz_left='1' or luz_right='1' or luz_back='1' then
-    estado <= ESTADO_SEGUIR_LUZ;
-end if;
-   
+                    -- BUSCAR_LUZ: robot detenido mientras espera luz
+                    when ESTADO_BUSCAR_LUZ =>
+                        leds_state <= "001";
+                        esquive_cnt <= 0;
 
-            -- ================================================
-            -- SEGUIR_LUZ
-            -- ================================================
-            when ESTADO_SEGUIR_LUZ =>
-    leds_state <= "010";
+                        mot_l_a <= '0';
+                        mot_l_b <= '0';
+                        mot_r_a <= '0';
+                        mot_r_b <= '0';
 
-    if luz_front='1' and luz_left='1' and luz_right='1' then
-        estado  <= ESTADO_META;
-        mot_l_a <= '0'; mot_l_b <= '0';
-        mot_r_a <= '0'; mot_r_b <= '0';
+                        if luz_front = '1' or luz_left = '1' or
+                           luz_right = '1' or luz_back = '1' then
+                            estado <= ESTADO_SEGUIR_LUZ;
+                        end if;
 
-    -- El que va a la izquierda ahora va al frente
-    elsif luz_left = '1' then
-        mot_l_a <= '1'; mot_l_b <= '0';
-        mot_r_a <= '1'; mot_r_b <= '0';
+                    -- SEGUIR_LUZ
+                    when ESTADO_SEGUIR_LUZ =>
+                        leds_state <= "010";
+                        esquive_cnt <= 0;
 
-    -- El que va en reversa ahora va a la izquierda
-    elsif luz_back = '1' then
-        mot_l_a <= '0'; mot_l_b <= '1';
-        mot_r_a <= '1'; mot_r_b <= '0';
+                        if luz_front = '1' and luz_left = '1' and luz_right = '1' then
+                            estado  <= ESTADO_META;
+                            mot_l_a <= '0'; mot_l_b <= '0';
+                            mot_r_a <= '0'; mot_r_b <= '0';
 
-    -- El que va al frente ahora gira a la derecha
-    elsif luz_front = '1' then
-        mot_l_a <= '1'; mot_l_b <= '0';
-        mot_r_a <= '0'; mot_r_b <= '1';
+                        elsif luz_left = '1' then
+                            mot_l_a <= '1'; mot_l_b <= '0';
+                            mot_r_a <= '1'; mot_r_b <= '0';
 
-    -- El de la derecha se queda igual
-    elsif luz_right = '1' then
-        mot_l_a <= '0'; mot_l_b <= '1';
-        mot_r_a <= '0'; mot_r_b <= '1';
+                        elsif luz_back = '1' then
+                            mot_l_a <= '0'; mot_l_b <= '1';
+                            mot_r_a <= '1'; mot_r_b <= '0';
 
-    else
-        estado  <= ESTADO_BUSCAR_LUZ;
+                        elsif luz_front = '1' then
+                            mot_l_a <= '1'; mot_l_b <= '0';
+                            mot_r_a <= '0'; mot_r_b <= '1';
 
-        mot_l_a <= '0';
-        mot_l_b <= '0';
-        mot_r_a <= '0';
-        mot_r_b <= '0';
-    end if;
+                        elsif luz_right = '1' then
+                            mot_l_a <= '0'; mot_l_b <= '1';
+                            mot_r_a <= '0'; mot_r_b <= '1';
 
-            -- ================================================
-            -- META: parado
-            -- ================================================
-            when ESTADO_META =>
-                leds_state <= "111";
-                mot_l_a    <= '0'; mot_l_b <= '0';
-                mot_r_a    <= '0'; mot_r_b <= '0';
+                        else
+                            estado  <= ESTADO_BUSCAR_LUZ;
+                            mot_l_a <= '0';
+                            mot_l_b <= '0';
+                            mot_r_a <= '0';
+                            mot_r_b <= '0';
+                        end if;
 
-                if luz_front='0' and luz_left='0' and luz_right='0' then
-                    estado <= ESTADO_BUSCAR_LUZ;
-                end if;
+                    -- ESQUIVE:
+                    -- Inicia desde que ultrasonico detecta obstaculo.
+                    when ESTADO_ESQUIVE =>
+                        leds_state <= "011";
 
-            when others =>
-                estado  <= ESTADO_INICIO;
-                mot_l_a <= '0'; mot_l_b <= '0';
-                mot_r_a <= '0'; mot_r_b <= '0';
+                        -- Solo motor izquierdo activo hacia adelante.
+                        -- Motor derecho apagado.
+                        mot_l_a <= '1';
+                        mot_l_b <= '0';
+                        mot_r_a <= '0';
+                        mot_r_b <= '0';
 
-            end case;
+                        if esquive_cnt >= ESQUIVE_MAX - 1 then
+                            esquive_cnt <= 0;
+
+                            if luz_front = '1' or luz_left = '1' or
+                               luz_right = '1' or luz_back = '1' then
+                                estado <= ESTADO_SEGUIR_LUZ;
+                            else
+                                estado <= ESTADO_BUSCAR_LUZ;
+                            end if;
+                        else
+                            esquive_cnt <= esquive_cnt + 1;
+                        end if;
+
+                    -- META: parado
+                    when ESTADO_META =>
+                        leds_state <= "111";
+                        esquive_cnt <= 0;
+
+                        mot_l_a <= '0'; mot_l_b <= '0';
+                        mot_r_a <= '0'; mot_r_b <= '0';
+
+                        if luz_front = '0' and luz_left = '0' and luz_right = '0' then
+                            estado <= ESTADO_BUSCAR_LUZ;
+                        end if;
+
+                    when others =>
+                        estado      <= ESTADO_INICIO;
+                        esquive_cnt <= 0;
+                        mot_l_a     <= '0';
+                        mot_l_b     <= '0';
+                        mot_r_a     <= '0';
+                        mot_r_b     <= '0';
+                        leds_state  <= "000";
+
+                end case;
+            end if;
         end if;
     end process;
 
